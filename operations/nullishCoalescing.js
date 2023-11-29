f7694c1a80574ba4f6e97615ad58b88793cef5fd
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-11-29 14:49:54 -0400

    refactor: Refatorando e melhorando código do operador Nullish Coalescing

--- operations/nullishCoalescing.js
+++ operations/nullishCoalescing.js
@@ -1,19 +1,17 @@
 // Nullish Coalescing Operator
+
 function mostrarIdade() {
   const idade1 = 27;
   const idade2 = null;
 
-  const resultadoDiv = document.getElementById("resultado");
-
-  resultadoDiv.innerText = `
-    A idade 01 é -> ${idade1 || "Não informado"} \n\n A idade 02 é -> ${
-    idade2 ?? "Não informado"
-  }`;
-
-  // (idade1 || "Não informado")
+  const idade1Result = `A idade 01 é: ${idade1 || "Não informado"}`;
   // 0, '', [], false, undefined, null => falsy
 
-  // (idade2 ?? "Não informado")
+  const idade2Result = `A idade 02 é: ${idade2 ?? "Não informado"}`;
   // mais restritivo então irá olhar para valores como
   // null, undefined
+
+  // Retornar os resultados como uma string
+  const resultadoDiv = document.getElementById("resultado");
+  resultadoDiv.innerText = `${idade1Result}\n\n${idade2Result}`;
 }
